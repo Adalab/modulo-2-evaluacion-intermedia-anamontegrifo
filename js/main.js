d93001d6adb705e4a0ac96e898474ac4db6fac3a
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-01 10:24:50 +0200

    add guessTheNumber function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 'use strict'
 //Variables containing the HTML elements used
-const selectedNumber = document.querySelector('js-selectedNumber');
+const selectedNumber = document.querySelector('.js-selectedNumber');
 const tryItBtn = document.querySelector('.js-try');
 const clueText = document.querySelector('.js-clueText');
 const triesText = document.querySelector('js-triesText');
+
 
 //Variable containing the generated random number
 function getRandomNumber(max) {
@@ -13,13 +14,35 @@ let randomNumber = getRandomNumber(100);
 console.log(randomNumber);
 
 
+//Function - guess the number
+function guessTheNumber() {
+
+    let selectedNumberValue = parseInt(selectedNumber.value);
+    if (selectedNumberValue === randomNumber) {
+        clueText.innerHTML = '¡Has ganado, campeona!'
+    }
+    else if (selectedNumberValue < 0 || selectedNumberValue > 100) {
+        clueText.innerHTML = 'El número debe estar entre 1 y 100';
+    }
+    else if (selectedNumberValue < randomNumber && selectedNumberValue <= 100) {
+        clueText.innerHTML = 'Demasiado bajo.'
+    }
+    else if (selectedNumberValue > randomNumber && selectedNumberValue <= 100) {
+        clueText.innerHTML = 'Demasiado alto.'
+    }
+    else {
+        clueText.innerHTML = 'El valor introducido debe ser un número entre 0 y 100'
+    }
+}
+
 
 //Handler function
+function handleTryIt() {
+    guessTheNumber();
 
-
-
+}
 
 
 
 //Listener
-// tryItBtn.addEventListener('click', handleTryIt);
+tryItBtn.addEventListener('click', handleTryIt);
