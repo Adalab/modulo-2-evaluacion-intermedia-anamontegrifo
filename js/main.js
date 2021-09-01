a86690a445fe0f4ae41ce57152f8eaa3c303e70f
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-01 09:24:51 +0200

    HTML variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 'use strict'
+//Variables containing the HTML elements used
+const selectedNumber = document.querySelector('js-selectedNumber');
+const tryItBtn = document.querySelector('.js-try');
+const clueText = document.querySelector('.js-clueText');
+const triesText = document.querySelector('js-triesText');
+
+//Variable containing the generated random number
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+let randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+
+
+
+//Handler function
+
+
+
+
+
+
+//Listener
+// tryItBtn.addEventListener('click', handleTryIt);
